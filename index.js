656fafa6fe766f325ad793fd58bd1d754a8d03f8
Jimm <chipndaletale@gmail.com>
2023-11-05 15:58:55 -0500

    Fifth commit

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: ["http://localhost:4000"],
+    origin: ["http://localhost:4000/login"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })

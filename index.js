d9c71a4d2753ac466ebec1f7c4bc3e78941b6cf1
Jimm <chipndaletale@gmail.com>
2023-11-05 15:05:39 -0500

    Fifth commit

--- index.js
+++ index.js
@@ -36,13 +36,8 @@ app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
+
 app.use(cookieParser());
 
 app.use(express.json());

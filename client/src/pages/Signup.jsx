d5cebeaeed0327e2f4f616732232f157f881e1d0
Jimm <chipndaletale@gmail.com>
2023-11-06 06:06:15 -0500

    Fifth commit

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -32,7 +32,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/signup",
+        "https://mern-to-heroku-2a747aaa3f64.herokuapp.com/signup",
         {
           ...inputValue,
         },

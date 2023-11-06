e2a14fde41bc6000c5cba2c5cebdead076fe9114
Jimm <chipndaletale@gmail.com>
2023-11-06 05:34:11 -0500

    Fifth commit

--- index.js
+++ index.js
@@ -19,17 +19,14 @@ mongoose
 // --------------------------deployment------------------------------
 //const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"))
   });
-}
+
+
 // --------------------------deployment------------------------------
 
 app.listen(PORT, () => {
